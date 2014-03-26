52204d28f39d712558460348c43ce3b189a03206
Jack <jacktimblin@gmail.com>
2014-03-26 15:02:34 +0000

    Completed BaseListener generation method

--- core/generation/ClassGeneration.java
+++ core/generation/ClassGeneration.java
@@ -1,8 +1,11 @@
 package generation;
 
 import java.io.File;
-import main.Application;
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroupFile;
 /**
  * this class will be used to generate a listener for a 
  * given language which uses all of the installed metrics.
@@ -10,7 +13,7 @@ import main.Application;
  */
 public class ClassGeneration {
    
-    public static void generateBaseListener(String grammarName) {
+    public static void generateBaseListener(String grammarName) throws IOException {
         
         //check that a listener has not already been generated.
         File listenerName = new File("CodeAnalyser/languages/"+grammarName+"/BaseListener.java");
@@ -19,6 +22,38 @@ public class ClassGeneration {
         }
         
         //generate the java class.
+        STGroupFile groupFile = new STGroupFile("CodeAnalyser/antlr/templates/ListenerGenerator.stg");
+        ST main = groupFile.getInstanceOf("main");
+        
+        //sort class properties.
+        ArrayList<String> imports = new ArrayList<String>();
+        imports.add("main.MetricInterface");
+        imports.add("main.Application");
+        imports.add("java.util.ArrayList");
+        imports.add("core.MetricException");
+        main.add("property", new Property(grammarName, imports, grammarName+"BaseListener"));
+        
+        //sort class variables.
+        ArrayList<VariableProperty> vars = new ArrayList<VariableProperty>();
+        vars.add(new VariableProperty("metrics", "ArrayList<MetricInterface>"));
+        
+        main.add("variables", vars);
+        
+        //sort the methods.
+        ArrayList<MethodProperty> methodNames = new ArrayList<MethodProperty>();
+        String m1 = "enterMethodBody"; String m2 = "enterMethodDeclaration";
+        String m3 = "enterClassDeclaration";
+        methodNames.add(new MethodProperty(m1, grammarName+"Parser."+m1.substring(5)+"Context"));
+        methodNames.add(new MethodProperty(m2, grammarName+"Parser."+m2.substring(5)+"Context"));
+        methodNames.add(new MethodProperty(m3, grammarName+"Parser."+m3.substring(5)+"Context"));
+        
+        main.add("methodProperties", methodNames);
+        
+        //save to a new file.
+        FileWriter writer = new FileWriter(new File("CodeAnalyser/languages/"+grammarName, "BaseListener.java"));
+        writer.append(main.render());
+        writer.flush();
+        writer.close();
         
     }
     
