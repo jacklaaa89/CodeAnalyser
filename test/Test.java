5ae1b1fa80c89b14036cc4c1105624d455b05197
Jack <jacktimblin@gmail.com>
2014-03-30 17:39:40 +0100

    Added tester java file to analyse.

---
+++ test/Test.java
@@ -0,0 +1,16 @@
+
+/**
+ * a random class that can be used to test metric evaluation.
+ * @author Jack
+ */
+public class Test {
+    
+    public void testerMethod(String tester1, String tester2) {
+        if(tester1.equals(tester2)) {
+            System.out.println(tester1);
+        } else {
+            System.out.println(tester2);
+        }
+    }
+    
+}
