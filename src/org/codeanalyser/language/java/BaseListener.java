5cefeaadfd5d685e3d1186ca6a027552797dc515
Jack <jacktimblin@gmail.com>
2014-03-31 11:47:56 +0100

    Updated auto-generated BaseListener

--- src/org/codeanalyser/language/java/BaseListener.java
+++ src/org/codeanalyser/language/java/BaseListener.java
@@ -1,31 +1,32 @@
 package org.codeanalyser.language.java;
 
-import java.util.ArrayList;
+import org.codeanalyser.metric.MetricInterface;
 import org.codeanalyser.core.Application;
-import org.codeanalyser.core.analyser.FileAnalyser;
+import java.util.ArrayList;
+import org.codeanalyser.language.MetricException;
 import org.codeanalyser.language.EventState;
 import org.codeanalyser.language.EventType;
 import org.codeanalyser.language.ListenerInterface;
-import org.codeanalyser.language.MetricException;
-import org.codeanalyser.metric.MetricInterface;
 import org.codeanalyser.metric.Result;
+import org.codeanalyser.core.analyser.FileAnalyser;
 
 /**
- * This class is auto generated when the parser is generated so that metrics
- * have a single class for each language that listens of all events that are
- * applicable for all metrics being tested.
+ * This class is auto generated when the parser is generated so that 
+ * metrics have a single class for each language that listens of all
+ * events that are applicable for all metrics being tested.
  *
- * because all metrics implement the MetricInterface interface, this means that
- * we can just call the start() method in this to invoke all methods.
- *
+ * because all metrics implement the MetricInterface interface, 
+ * this means that we can just call the start() method in this to invoke 
+ * all methods.
+ * 
  * {@link MetricInterface.start() }
- *
  * @author Jack Timblin - U1051575 (via StringTemplate4)
  */
 public class BaseListener extends JavaBaseListener implements ListenerInterface {
 
     private ArrayList<MetricInterface> metrics;
     private FileAnalyser file;
+
 
     @Override
     public void init(FileAnalyser file) throws MetricException {
@@ -45,13 +46,12 @@ public class BaseListener extends JavaBaseListener implements ListenerInterface 
 
     /**
      * gathers the results from the metrics in this listener.
-     *
      * @return an ArrayList<Result> of string results from each of the metrics.
      */
     @Override
     public ArrayList<Result> getResults() {
         ArrayList<Result> results = new ArrayList<Result>();
-        for (MetricInterface metric : metrics) {
+        for(MetricInterface metric : metrics) {
             results.add(metric.getResults());
         }
         return results;
@@ -59,70 +59,60 @@ public class BaseListener extends JavaBaseListener implements ListenerInterface 
 
     /**
      * generated method to call enterMethodBody while walking the parse tree.
-     *
-     * @param context
-     * <p>
-     * The context/area of the parse tree that this rule applies to.</p>
+     * @param context <p>The context/area of the parse tree that this rule applies to.</p>
      */
-    @Override
-    public void enterMethodBody(JavaParser.MethodBodyContext context) {
+     @Override
+     public void enterMethodBody(JavaParser.MethodBodyContext context) {
         //build state object.
         EventState.EventStateBuilder builder = new EventState.EventStateBuilder();
         EventState state = builder.setContext(context)
-                .setSourceLanguage("java")
-                .setEventType(EventType.ENTER_METHOD_BODY)
-                .setFileName(file.getAbsolutePath())
-                .build();
-        for (MetricInterface metric : metrics) {
+        			      .setSourceLanguage("java")
+        			      .setEventType(EventType.ENTER_METHOD_BODY)
+                                  .setFileName(file.getAbsolutePath())
+        			      .build();
+        for(MetricInterface metric : metrics) {
             //start the metrics evaluation at this event.
             metric.start(state);
         }
-    }
-
+     }
+     
     /**
-     * generated method to call enterMethodDeclaration while walking the parse
-     * tree.
-     *
-     * @param context
-     * <p>
-     * The context/area of the parse tree that this rule applies to.</p>
+     * generated method to call enterMethodDeclaration while walking the parse tree.
+     * @param context <p>The context/area of the parse tree that this rule applies to.</p>
      */
-    @Override
-    public void enterMethodDeclaration(JavaParser.MethodDeclarationContext context) {
+     @Override
+     public void enterMethodDeclaration(JavaParser.MethodDeclarationContext context) {
         //build state object.
         EventState.EventStateBuilder builder = new EventState.EventStateBuilder();
         EventState state = builder.setContext(context)
-                .setSourceLanguage("java")
-                .setFileName(file.getAbsolutePath())
-                .setEventType(EventType.ENTER_METHOD_DECLARATION)
-                .build();
-        for (MetricInterface metric : metrics) {
+        			      .setSourceLanguage("java")
+        			      .setEventType(EventType.ENTER_METHOD_DECLARATION)
+                                  .setFileName(file.getAbsolutePath())
+        			      .build();
+        for(MetricInterface metric : metrics) {
             //start the metrics evaluation at this event.
             metric.start(state);
         }
-    }
-
+     }
+     
     /**
-     * generated method to call enterClassDeclaration while walking the parse
-     * tree.
-     *
-     * @param context
-     * <p>
-     * The context/area of the parse tree that this rule applies to.</p>
+     * generated method to call enterClassDeclaration while walking the parse tree.
+     * @param context <p>The context/area of the parse tree that this rule applies to.</p>
      */
-    @Override
-    public void enterClassDeclaration(JavaParser.ClassDeclarationContext context) {
+     @Override
+     public void enterClassDeclaration(JavaParser.ClassDeclarationContext context) {
         //build state object.
         EventState.EventStateBuilder builder = new EventState.EventStateBuilder();
         EventState state = builder.setContext(context)
-                .setSourceLanguage("java")
-                .setFileName(file.getAbsolutePath())
-                .setEventType(EventType.ENTER_CLASS_DECLARATION)
-                .build();
-        for (MetricInterface metric : metrics) {
+        			      .setSourceLanguage("java")
+        			      .setEventType(EventType.ENTER_CLASS_DECLARATION)
+                                  .setFileName(file.getAbsolutePath())
+        			      .build();
+        for(MetricInterface metric : metrics) {
             //start the metrics evaluation at this event.
             metric.start(state);
         }
-    }
+     }
+     
 
 }
