b6b102f4c42e117e571624b79d81010a22814ec9
Jack <jacktimblin@gmail.com>
2014-03-31 10:21:24 +0100

    Removed unwanted imports from FileAnalyser

--- src/org/codeanalyser/core/analyser/FileAnalyser.java
+++ src/org/codeanalyser/core/analyser/FileAnalyser.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.codeanalyser.core.analyser;
 
 import java.io.File;
@@ -11,7 +5,6 @@ import java.lang.reflect.Constructor;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Lexer;
-import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.apache.commons.io.FilenameUtils;
