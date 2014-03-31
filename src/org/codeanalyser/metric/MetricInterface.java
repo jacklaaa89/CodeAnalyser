c54308bdd9e90e3046f81c095e5451244e99fbea
Jack <jacktimblin@gmail.com>
2014-03-31 11:38:36 +0100

    Added init to MetricInterface so that initial values can be pushed to metrics, also used Result Object

--- src/org/codeanalyser/metric/MetricInterface.java
+++ src/org/codeanalyser/metric/MetricInterface.java
@@ -1,21 +1,19 @@
 package org.codeanalyser.metric;
 
-import org.antlr.v4.runtime.ParserRuleContext;
 import org.codeanalyser.language.EventState;
 
 /**
  * this is the interface that all metrics should implement.
  * @author Jack
- * @param <T> This is any class that is a context from the parser.
  */
-public interface MetricInterface<T extends ParserRuleContext> {
+public interface MetricInterface {
     
     /**
      * returns the results from this metric, this will
      * be the results when this metric is run on a single class.
      * @return the results from running the metric.
      */
-    public String getResults();
+    public Result getResults();
     
     /**
      * starts this metrics evaluation of a 
@@ -23,5 +21,13 @@ public interface MetricInterface<T extends ParserRuleContext> {
      * @param state the state of the event.
      */
     public void start(EventState state);
+    
+    /**
+     * give the metric initial information regarding the file
+     * it is evaluating.
+     * @param fileLocation the absolute file location for the file being evaluated.
+     * @param sourceLanguage the source language of the file.
+     */
+    public void init(String fileLocation, String sourceLanguage);
     
 }
