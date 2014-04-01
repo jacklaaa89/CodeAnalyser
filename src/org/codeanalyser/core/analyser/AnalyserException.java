d300b759602c1a63fbecaffa33fe02a722d5144b
Jack <jacktimblin@gmail.com>
2014-04-01 12:27:57 +0100

    Added AnalyserException, which is thrown when Analyser errors occur.

---
+++ src/org/codeanalyser/core/analyser/AnalyserException.java
@@ -0,0 +1,13 @@
+package org.codeanalyser.core.analyser;
+
+/**
+ * Exception that is thrown when Analyser errors occur.
+ * @author Jack Timblin - U1051575
+ */
+class AnalyserException extends Exception {
+
+    public AnalyserException(String message) {
+        super(message);
+    }
+    
+}
