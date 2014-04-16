7b169694139832cd5f5e64aff5f9d0a14042193f
Jack Timblin <jack.timblin@partsit.com>
2014-04-16 16:11:19 +0100

    Made AnalyserException public so we can use it in Main

--- src/org/codeanalyser/core/analyser/AnalyserException.java
+++ src/org/codeanalyser/core/analyser/AnalyserException.java
@@ -4,7 +4,7 @@ package org.codeanalyser.core.analyser;
  * Exception that is thrown when Analyser errors occur.
  * @author Jack Timblin - U1051575
  */
-class AnalyserException extends Exception {
+public class AnalyserException extends Exception {
 
     public AnalyserException(String message) {
         super(message);
