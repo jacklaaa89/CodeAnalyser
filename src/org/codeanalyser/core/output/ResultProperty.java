fe391eefe049df7ea00d8a411d0ef98e5f6277c2
Jack Timblin <jack.timblin@partsit.com>
2014-04-16 12:12:59 +0100

    Added commenting to ResultProperty class

--- src/org/codeanalyser/core/output/ResultProperty.java
+++ src/org/codeanalyser/core/output/ResultProperty.java
@@ -15,7 +15,7 @@ public class ResultProperty {
     /**
      * the result HTML passed from the metric
      */
-    private String resultHTML;
+    private String link;
     /**
      * the color code to make the background of the result box
      * red if the file failed the metric, green if it passed.
@@ -34,31 +34,60 @@ public class ResultProperty {
      */
     private String successMessage;
     
+    /**
+     * gets the message SUCCESS or FAILED dependant on 
+     * how well this file performed.
+     * @return the success message.
+     */
     public String getSuccessMessage() {
         return this.successMessage;
     }
     
-    public String getResultHTML() {
-        return this.resultHTML;
+    /**
+     * gets the HTML href link to link to the subpage.
+     * @return the href link.
+     */
+    public String getLink() {
+        return this.link;
     }
     
+    /**
+     * gets the absolute file name for the file that was analysed.
+     * @return the filename.
+     */
     public String getFileName() {
         return this.fileName;
     }
     
+    /**
+     * gets the source language of the file.
+     * @return the source language.
+     */
     public String getSourceLanguage() {
         return this.sourceLanguage;
     }
     
+    /**
+     * gets the color code to use dependant on 
+     * how well this file performed.
+     * @return the color code to use.
+     */
     public String getColorCode() {
         return this.colorCode;
     }
     
-    public ResultProperty(String resultsHTML, boolean isSuccessful, 
+    /**
+     * initialises a ResultProperty and uses contructor overloading.
+     * @param link the href link.
+     * @param isSuccessful if this file was mainly successful or not.
+     * @param fileName the absolute file name of this file.
+     * @param sourceLanguage the source language of this file.
+     */
+    public ResultProperty(String link, boolean isSuccessful, 
             String fileName, String sourceLanguage) {
         this.colorCode = isSuccessful ? "green" : "red";
         this.fileName = fileName;
-        this.resultHTML = resultsHTML;
+        this.link = link;
         this.sourceLanguage = sourceLanguage;
         this.successMessage = isSuccessful ? "Passed" : "Failed";
     }
