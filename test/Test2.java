ceb720b02367da40909c83b9c52df3db30537165
Jack Timblin <jack.timblin@partsit.com>
2014-04-16 16:14:26 +0100

    Renamed Tester File to test if the file is the same

--- test/Test2.java
+++ test/Test2.java
@@ -3,7 +3,7 @@
  * a random class that can be used to test metric evaluation.
  * @author Jack Timblin - U1051575
  */
-public class Test {
+public class Test2 {
     
     /**
      * A random tester method.
@@ -18,7 +18,20 @@ public class Test {
         }
     }
     
-    public Test() {
+    /**
+     * A random tester method.
+     * @param tester1 a random parameter.
+     * @param tester2 another random parameter.
+     */
+    public void testerMethod2(String tester1, String tester2) {
+        if(tester1.equals(tester2)) {
+            System.out.println(tester1);
+        } else {
+            System.out.println(tester2);
+        }
+    }
+    
+    public Test2() {
         //tester constructor.
     }
     
