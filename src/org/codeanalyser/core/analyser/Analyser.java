bb6502727277502f35395f2b54e1f3b2ba269076
Jack Timblin <jack.timblin@partsit.com>
2014-04-16 12:05:27 +0100

    Updated Analyser to use OverallResult instead of ArrayList<Result>

--- src/org/codeanalyser/core/analyser/Analyser.java
+++ src/org/codeanalyser/core/analyser/Analyser.java
@@ -1,31 +1,28 @@
 package org.codeanalyser.core.analyser;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.codeanalyser.core.output.HtmlParserException;
 import org.codeanalyser.core.output.NoResultsDefinedException;
 import org.codeanalyser.core.output.OutputGenerator;
-import org.codeanalyser.core.output.TemplateNotFoundException;
+import org.codeanalyser.core.output.OverallResult;
 import org.codeanalyser.language.ListenerInterface;
 import org.codeanalyser.language.ParserInterface;
-import org.codeanalyser.metric.Result;
 
 /**
  * This class is the one that analyses the file/directory that is the input.
  * It determines the supported listener, parser and lexer from a single file and then walks
  * that tree notifying the listener when events occur.
  * @author Jack Timblin - U1051575  
+ * @version 1.1 - updated analyse to use OverallResult instead of Result for output generation.
  */
 public class Analyser {
     
     private File sourceCodeLocation;
-    private ArrayList<ArrayList<Result>> results;
+    private ArrayList<OverallResult> results;
     private ArrayList<FileAnalyser> filesToAnalyse;
     private OutputGenerator output;
     private ArrayList<String> unsupportedFiles;
@@ -33,10 +30,11 @@ public class Analyser {
     /**
      * initialises the analyser object with a file/directory location.
      * @param sourceCodeLocation the file or directory to analyse.
+     * @throws AnalyserException if the given source code location does not exist.
      */
     public Analyser(String sourceCodeLocation, String outputLocation) throws AnalyserException {
         this.sourceCodeLocation = new File(sourceCodeLocation);
-        this.results = new ArrayList<ArrayList<Result>>();
+        this.results = new ArrayList<OverallResult>();
         this.filesToAnalyse = new ArrayList<FileAnalyser>();
         this.unsupportedFiles = new ArrayList<String>();
         if(!this.sourceCodeLocation.exists()) {
@@ -71,30 +69,39 @@ public class Analyser {
     public void analyse() {
         for(FileAnalyser file : this.filesToAnalyse) {
             try {
+                
+                //generate parse tree from source file.
                 Lexer lexer = file.getSupportedLexer();
                 CommonTokenStream tokens = new CommonTokenStream(lexer);
                 ParserInterface parser = file.getSupportedParser(tokens);
                 ParserRuleContext tree = parser.compilationUnit();
                 ParseTreeListener listener = file.getSupportedListener(parser.getTokenNames());
                 ParseTreeWalker walker = new ParseTreeWalker();
+                
+                //walk the parse tree calling methods in the metrics.
                 walker.walk(listener, tree);
-
-                this.results.add(((ListenerInterface) listener).getResults());
+                
+                //gather the results from the analysis.
+                OverallResult result = new OverallResult(
+                        ((ListenerInterface) listener).getResults(),
+                        file.getAbsolutePath()
+                );
+                
+                //add this to result array.
+                this.results.add(result);
 
             } catch (FileAnalyser.UnsupportedLanguageException e) {
                 this.unsupportedFiles.add(file.getAbsolutePath());
+                System.out.println(e.getMessage());
+            } catch (NoResultsDefinedException e) {
                 System.out.println(e.getMessage());
             }
         }
         try {
             //render the output for this analysis.
             this.output.generateOutput(this.results, this.unsupportedFiles);
-        } catch (NoResultsDefinedException ex) {
-            System.out.println("No Results were generated when performing the metrics.");
-        } catch (TemplateNotFoundException ex) {
-            System.out.println(ex.getMessage());
-        } catch (HtmlParserException ex) {
-            System.out.println("Could not parse Result String into HTML");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
     
