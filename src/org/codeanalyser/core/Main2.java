e6ad7cf0e12bbc8c93c1a3d163d36ce55e884faf
Jack <jacktimblin@gmail.com>
2014-04-21 21:20:14 +0100

    Added secondary tester Main class 'Main2' which can be used to run tester code.

---
+++ src/org/codeanalyser/core/Main2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.codeanalyser.core;
+
+import org.codeanalyser.core.analyser.LanguageDetect;
+
+/**
+ *
+ * @author jack
+ */
+public class Main2 {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
