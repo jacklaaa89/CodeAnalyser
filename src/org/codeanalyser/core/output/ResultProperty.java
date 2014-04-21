c44c62ab8f285984c8a8f1dffdc1c2a1342132b8
Jack <jacktimblin@gmail.com>
2014-04-21 18:36:08 +0100

    Added commenting to ResultProperty

--- src/org/codeanalyser/core/output/ResultProperty.java
+++ src/org/codeanalyser/core/output/ResultProperty.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.codeanalyser.core.output;
 
 /**
- *
- * @author jack
+ * A representation of an overall result that is used in 
+ * the output template.
+ * @author Jack Timblin - U1051575
  */
 public class ResultProperty {
     
