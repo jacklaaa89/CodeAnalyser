1a8a29d0dacaccbf60b92de0140a2069719c59c6
Jack <jacktimblin@gmail.com>
2014-04-21 18:35:49 +0100

    Added commenting to TemplateNotFoundException

--- src/org/codeanalyser/core/output/TemplateNotFoundException.java
+++ src/org/codeanalyser/core/output/TemplateNotFoundException.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.codeanalyser.core.output;
 
 /**
- *
- * @author jack
+ * Simple Exception class for when Output IO Exceptions occur.
+ * @author Jack Timblin - U1051575
  */
 public class TemplateNotFoundException extends Exception {
     public TemplateNotFoundException(String message) {
