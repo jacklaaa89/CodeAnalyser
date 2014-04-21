c9f57f70cce1fe7cd6b3374d84233f7e138561c0
Jack <jacktimblin@gmail.com>
2014-04-21 18:33:10 +0100

    Added commenting to Metric Exceptions

--- src/org/codeanalyser/metric/InvalidResultException.java
+++ src/org/codeanalyser/metric/InvalidResultException.java
@@ -1,8 +1,9 @@
 package org.codeanalyser.metric;
 
 /**
- *
- * @author Jack
+ * This exception is thrown in any getResults() method call in a metric when the
+ * result is not valid.
+ * @author Jack Timblin - U1051575
  */
 public class InvalidResultException extends Exception {
     public InvalidResultException(String message) {
--- src/org/codeanalyser/metric/MetricInitialisationException.java
+++ src/org/codeanalyser/metric/MetricInitialisationException.java
@@ -1,8 +1,9 @@
 package org.codeanalyser.metric;
 
 /**
- *
- * @author Jack
+ * Exception thrown by init() in the metric classes when the metric
+ * cannot be initialised.
+ * @author Jack Timblin - U1051575
  */
 public class MetricInitialisationException extends Exception {
     public MetricInitialisationException(String message) {
