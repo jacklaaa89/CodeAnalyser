a07d022c8f1f0b4d130858461b2e4fae1c597628
Jack <jacktimblin@gmail.com>
2014-04-21 18:34:31 +0100

    Added commenting to MetricException

--- src/org/codeanalyser/language/MetricException.java
+++ src/org/codeanalyser/language/MetricException.java
@@ -1,8 +1,8 @@
 package org.codeanalyser.language;
 
 /**
- *
- * @author Jack
+ * Used when any Exception occurs when any error occurs using metrics.
+ * @author Jack Timblin - U1051575
  */
 public class MetricException extends Exception {
     
