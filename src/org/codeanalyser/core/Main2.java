e4125072addcb597d6ecd7e8bd249357e8952e4b
Jack <jacktimblin@gmail.com>
2014-04-23 00:17:54 +0100

    CHanged Main2 to run the new Hello Parser/Lexer in the test file and display the parse tree.

--- src/org/codeanalyser/core/Main2.java
+++ src/org/codeanalyser/core/Main2.java
@@ -1,5 +1,15 @@
 package org.codeanalyser.core;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.codeanalyser.core.generation.ClassGeneration;
+import org.codeanalyser.language.hello.HelloLexer;
+import org.codeanalyser.language.hello.HelloParser;
+
 
 /**
  *
@@ -8,7 +18,16 @@ package org.codeanalyser.core;
 public class Main2 {
     
     public static void main(String[] args) {
-        
+        try {
+            HelloLexer lexer = new HelloLexer(new ANTLRFileStream("test/Test.hello"));
+            CommonTokenStream stream = new CommonTokenStream(lexer);
+            HelloParser parser = new HelloParser(stream);
+            //ClassGeneration.generateBaseListener("Hello", parser.getRuleNames());
+            ParserRuleContext tree = parser.compilationUnit();
+            tree.inspect(parser);
+        } catch (IOException ex) {
+            Logger.getLogger(Main2.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
