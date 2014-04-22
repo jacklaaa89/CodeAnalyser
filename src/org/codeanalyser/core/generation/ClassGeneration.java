19dfce9e378b8febb10e7a722eb77122dc81d5f1
Jack <jacktimblin@gmail.com>
2014-04-22 11:28:26 +0100

    Fixed bug in ClassGeneration where the case of the grammar name would sometimes make it so the package to save the generated class could not be found

--- src/org/codeanalyser/core/generation/ClassGeneration.java
+++ src/org/codeanalyser/core/generation/ClassGeneration.java
@@ -9,14 +9,24 @@ import org.stringtemplate.v4.STGroupFile;
 /**
  * this class will be used to generate a listener for a 
  * given language which uses all of the installed metrics.
- * @author Jack
+ * @author Jack Timblin - U1051575
+ * @version 1.1 Added rule names to constructor so that the generator generates
+ * metric calls for all rules in the grammar.
+ * @version 1.1.1 Updated bug in the way the grammar saved the new file.
  */
 public class ClassGeneration {
-   
+    
+    /**
+     * Generates a BaseListener instance for a specified grammar.
+     * @param grammarName the name of the grammar.
+     * @param ruleNames the rules the grammar contains, these will be used
+     * to generate enter{RULE_NAME} methods so that metrics get called at every point.
+     * @throws IOException if the file does not exist or cannot be saved.
+     */
     public static void generateBaseListener(String grammarName, String[] ruleNames) throws IOException {
         
         //check that a listener has not already been generated.
-        File listenerName = new File("src/org/codeanalyser/language/"+grammarName+"/BaseListener.java");
+        File listenerName = new File("src/org/codeanalyser/language/"+grammarName.toLowerCase()+"/BaseListener.java");
         if(listenerName.exists()) {
             return;
         }
@@ -48,14 +58,10 @@ public class ClassGeneration {
         main.add("methodProperties", methodNames);
         
         //save to a new file.
-        FileWriter writer = new FileWriter(new File("src/org/codeanalyser/language/"+grammarName, "BaseListener.java"));
+        FileWriter writer = new FileWriter(new File("src/org/codeanalyser/language/"+grammarName.toLowerCase(), "BaseListener.java"));
         writer.append(main.render());
         writer.flush();
         writer.close();
-        
-    }
-    
-    public static void updateEventTypes(String[] ruleNames) {
         
     }
     
