942547f7a29bb218acc753a7fd2af35f4d38707b
Jack <jacktimblin@gmail.com>
2014-04-26 04:01:34 +0100

    Added ApplicationException when a runtime exception occurs on startup

---
+++ src/org/codeanalyser/core/ApplicationException.java
@@ -0,0 +1,11 @@
+package org.codeanalyser.core;
+
+/**
+ *
+ * @author jack
+ */
+public class ApplicationException extends Exception {
+    public ApplicationException(String message) {
+        super(message);
+    }
+}
