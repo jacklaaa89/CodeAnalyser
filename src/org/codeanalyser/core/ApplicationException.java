fd4e905c76dbb2c51b53b04f54bca037fabd0f6e
jacklaaa89 <jacktimblin@gmail.com>
2014-04-28 03:28:52 -0700

    Updated commenting in ApplicationException

--- src/org/codeanalyser/core/ApplicationException.java
+++ src/org/codeanalyser/core/ApplicationException.java
@@ -1,10 +1,14 @@
 package org.codeanalyser.core;
 
 /**
- *
- * @author jack
+ * Simple Exception for when errors occur getting Application properties.
+ * @author Jack Timblin - U1051575
  */
 public class ApplicationException extends Exception {
+    /**
+     * initialises a new ApplicationException
+     * @param message the message.
+     */
     public ApplicationException(String message) {
         super(message);
     }
